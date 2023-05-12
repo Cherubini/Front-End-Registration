5558385c0dabbf2b89b06dd6cd769f7e19ea35c1
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-05-12 09:37:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './style.css'
 
+
+export function reverseWords(str) {
+    let tempChars= str.split(''); 
+    let tempStr='';
+    let tempArr=[];
+    for(let i=0; i<tempChars.length; i++) {
+      let element =tempChars[i];
+      if(element !==' '  && i+1!=tempChars.length)
+        tempArr.push(element);
+       else 
+          {
+          tempStr+= tempArr.reverse().join('');
+          tempArr=[];
+          tempStr+=element;
+          }
+    }
+    return tempStr;
+      
+  }
+  console.log(reverseWords('The quick brown fox jumps over the lazy dog.')); 
